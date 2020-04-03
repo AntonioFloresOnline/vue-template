b289f4d5723165193f0f2882d7325337563fe154
antoniofloresonline <wanllyf@gmail.com>
2020-04-03 11:26:27 -0600

    Changing colors and fixing styles

--- resources/js/page1/components/VuexInput.vue
+++ resources/js/page1/components/VuexInput.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="component">
     <h3 class="component-title">
-      I am a Vuex Input, add some data.
+      I am a Vuex Input, please add some data.
     </h3>
     <div class="form-group">
       <label for="itemTitle">Title for new Item in data: </label>
-      <input v-model="newItem.title" id="itemTitle" type="text" />
+      <input
+        v-model="newItem.title"
+        id="itemTitle"
+        type="text"
+        placeholder="Type Something"
+      />
     </div>
 
     <div class="form-group">
       <label for="taskDescription">Description for new task: </label>
-      <input v-model="newItem.description" id="itemDescription" type="text" />
+      <input
+        v-model="newItem.description"
+        id="itemDescription"
+        type="text"
+        placeholder="Type Something"
+      />
     </div>
 
     <div class="form-group">
